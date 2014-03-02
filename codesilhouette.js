95e15188e59051f659e038f78d4cd0cc0e317938
Joshua Merriman <chainsaw.police@gmail.com>
2014-03-02 20:56:20 +1100

    First version

---
+++ codesilhouette.js
@@ -0,0 +1,45 @@
+canvas = document.getElementById('myCanvas');
+context = canvas.getContext('2d');
+
+var unitSize = 5; 
+var silhouetteColour = "#333";
+
+function drawSilhouette(){
+	s = $("#codeArea").val();
+
+	var lines = s.split("\n"); 
+	var allLineLengths = new Array(); 
+
+	var longestLine = 0
+	for (var i = 0; i < lines.length; i++) { 
+		allLineLengths.push(lines[i].length); 
+
+		if (lines[i].length > longestLine){
+			longestLine = lines[i].length
+		}
+	};
+
+	canvas.width  = Math.floor(longestLine * unitSize / 2)
+	canvas.height = allLineLengths.length * unitSize;
+
+	function drawLine(rectY, rectWidth){ // Draws a line of code for the silhouette, not an actual line stroke.
+		context.beginPath();
+		context.rect(0, rectY, rectWidth, unitSize);
+		context.fillStyle = silhouetteColour;
+		context.fill();
+	};
+
+	for (var i=0;i < allLineLengths.length;i++){
+		drawLine(
+			i * unitSize, // Line Y position. Increment 
+			Math.floor(allLineLengths[i] * unitSize / 2) 
+		);
+	};
+
+	var dataURL = canvas.toDataURL();
+
+	$("#canvasImg").css("display", "inline");
+	$("#permalinkWrapper").css("display", "inline");
+	document.getElementById('canvasImg').src = dataURL;
+	$("#permalink").attr("href", dataURL);	
+};
