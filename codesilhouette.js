e6b9b1aff3f10104f300e85728d67b5d55705670
minrice2099 <minrice2099@gmail.com>
2014-03-02 14:19:37 -0600

    'Fixed' some whitespace and semicolons
    While semicolons are almost always optional in JS, most people say you should use them.
    For more info on when and when not to use semicolons, see: 
    http://stackoverflow.com/questions/2717949/when-should-i-use-a-semicolon-after-curly-braces

--- codesilhouette.js
+++ codesilhouette.js
@@ -1,7 +1,7 @@
 var drawSilhouette = (function() {
 	var canvas = document.getElementById('myCanvas');
 	var context = canvas.getContext('2d');
-	var unitSize = 5; 
+	var unitSize = 5;
 	var silhouetteColour = "#333";
 	
 	// Draws a line of code for the silhouette, not an actual line stroke.
@@ -10,33 +10,33 @@ var drawSilhouette = (function() {
 		context.rect(0, rectY, rectWidth, unitSize);
 		context.fillStyle = silhouetteColour;
 		context.fill();
-	};
+	}
 
 	return function() {
 		s = document.getElementById("codeArea").value;
 	
 		// Strip carriage returns (Windows platform only), then split on new lines
-		var lines = s.replace(/\r/g, "").split("\n"); 
-		var allLineLengths = new Array(); 
+		var lines = s.replace(/\r/g, "").split("\n");
+		var allLineLengths = new Array();
 	
-		var longestLine = 0
-		for (var i = 0; i < lines.length; i++) { 
-			allLineLengths.push(lines[i].length); 
+		var longestLine = 0;
+		for (var i = 0; i < lines.length; i++) {
+			allLineLengths.push(lines[i].length);
 	
-			if (lines[i].length > longestLine){
-				longestLine = lines[i].length
+			if (lines[i].length > longestLine) {
+				longestLine = lines[i].length;
 			}
-		};
+		}
 	
-		canvas.width  = Math.floor(longestLine * unitSize / 2)
+		canvas.width  = Math.floor(longestLine * unitSize / 2);
 		canvas.height = allLineLengths.length * unitSize;
 	
-		for (var i=0;i < allLineLengths.length;i++){
+		for (var i = 0; i < allLineLengths.length; i++) {
 			drawLine(
 				i * unitSize, // Line Y position. Increment 
 				Math.floor(allLineLengths[i] * unitSize / 2) 
 			);
-		};
+		}
 	
 		var dataURL = canvas.toDataURL();
 	
